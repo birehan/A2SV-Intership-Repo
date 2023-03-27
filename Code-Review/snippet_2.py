37e8ad88a805391ed0561b9534765fa6cbe20b61
Gadisa Amenu <amegadbay@gmail.com>
2023-03-27 21:38:29 +0300

    fixed issue

--- Code-Review/snippet_2.py
+++ Code-Review/snippet_2.py
@@ -1,22 +1,23 @@
-def is_valid_parenthesis(s):
-    dc = {
+def is_valid_parenthesis(input_string):
+    # Map opening parentheses to their corresponding closing parentheses
+    parentheses_map = {
         '(': ')',
-          "{": "}",
-         '[': ']'
-
+        '{': '}',
+        '[': ']'
     }
-    stack = []
-    for i in s:
-        if i in dc.keys(): stack.append(i)
-        else:
-            if len(stack) == 0:
-                return False
-            
 
-            op = stack.pop()
-            if dc[op] != i:
-                return False
-    if len(stack) != 0:
-        return False
-    
-    return True
+    # Use a stack to keep track of opening parentheses
+    stack = []
+
+    # Iterate over each character in the input string
+    for char in input_string:
+        if char in parentheses_map.keys():
+            # If the character is an opening parenthesis, push it onto the stack
+            stack.append(char)
+        elif len(stack) == 0 or parentheses_map[stack.pop()] != char:
+            # If the character is a closing parenthesis, pop the top element from the stack
+            # and check if it matches the corresponding opening parenthesis
+            return False
+
+    # If the stack is not empty, there are unmatched opening parentheses
+    return len(stack) == 0
