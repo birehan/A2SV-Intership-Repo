45913b864eb299e7ae704b17ed9a92d6915add9d
BIrehan Anteneh <91595957+birehan@users.noreply.github.com>
2023-03-27 19:51:14 +0300

    Update string_functions.py

--- Testing-Assessment/string_functions.py
+++ Testing-Assessment/string_functions.py
@@ -1,3 +1,5 @@
+# Two error have been found and fixed
+
 def to_upper(string):
     result = []
 
@@ -11,19 +13,22 @@ def to_upper(string):
 
 def to_lower(string):
     result = []
-
     for char in string:
         if 65 <= ord(char) <= 90:
-            result.append(chr(ord(char) - 32))
+
+            # error on the below line fixed, 32 have to be plus
+            result.append(chr(ord(char) + 32))
+
         else:
             result.append(char)
-            
     return "".join(result)
 
 def capitalize(string):
     result = ""
+    
+    # error on the below line fixed, the if condition should be based on the value of string
+    if string:
 
-    if result:
         result += to_upper(string[0])
     result += to_lower(string[1:])
 
