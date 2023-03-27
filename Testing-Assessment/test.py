33ed8a448855cbb8e981f7142c866fa5960e6648
BIrehan Anteneh <91595957+birehan@users.noreply.github.com>
2023-03-27 19:51:48 +0300

    Update test.py

--- Testing-Assessment/test.py
+++ Testing-Assessment/test.py
@@ -3,15 +3,31 @@ from string_functions import *
 
 class TestToUpper(unittest.TestCase):
     # write your code here
-    pass
+    def test_upper(self):
+        self.assertEqual(to_upper("hello"), "HELLO")
+        self.assertEqual(to_upper("WORLD"), "WORLD")
+        self.assertEqual(to_upper("123"), "123")
+        self.assertEqual(to_upper(""), "")
+        self.assertEqual(to_upper("hElLo"), "HELLO")
 
 class TestToLower(unittest.TestCase):
+    def test_lower(self):
+        self.assertEqual(to_lower("HELLO"), "hello")
+        self.assertEqual(to_lower("world"), "world")
+        self.assertEqual(to_lower("123"), "123")
+        self.assertEqual(to_lower(""), "")
+        self.assertEqual(to_lower("HeLlO"), "hello")
     # Write you code here
-    pass
+  
 
 class TestCapitalize(unittest.TestCase):
     # Write your code here
-    pass
+    def test_capitalize(self):
+        self.assertEqual(capitalize("hello"), "Hello")
+        self.assertEqual(capitalize("WORLD"), "World")
+        self.assertEqual(capitalize("123"), "123")
+        self.assertEqual(capitalize(""), "")
+        self.assertEqual(capitalize("hElLo"), "Hello")
 
 if __name__ == '__main__':
     unittest.main()
